d3a5ffecdf1a365e39082d4fb571189d2474f1cd
gopal <gopal1409@gmail.com>
2024-06-14 12:34:39 +0530

    data block

--- resource.tf
+++ resource.tf
@@ -1,9 +1,14 @@
 #how to create a resource group. 
-resource "azurerm_resource_group" "myrg" { #this is the refrence it will be managed by terraform to store your logs
+/*resource "azurerm_resource_group" "myrg" { #this is the refrence it will be managed by terraform to store your logs
   name = "${local.resource_name_prefix}-${var.resource_group_name}"
   #sap-dev-rg-default
   location = var.resource_group_location
   tags     = local.common_tags
+}*/
+data "azurerm_resource_group" "myrg1" {
+  name = data.azurerm_resource_group.myrg.name
+  location  = data.azurerm_resource_group.myrg.location
+  
 }
 
 module "vnet" {
